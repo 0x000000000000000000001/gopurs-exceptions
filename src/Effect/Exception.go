5d4f4559f97532e07a82e750d77e79c6b64afbc6
0x1 <kevin.francart3@gmail.com>
2026-08-01 13:49:53 +0200

    fix(ffi): fix various FFI implementations

--- src/Effect/Exception.go
+++ src/Effect/Exception.go
@@ -1,24 +1,29 @@
+package Effect_Exception
+
 import "errors"
+
 func Error(msg string) error { return errors.New(msg) }
 func Message(e error) string { return e.Error() }
 func Name(e error) string { return "Error" }
 func ShowErrorImpl(e error) string { return e.Error() }
 func StackImpl(just func(string) interface{}, nothing interface{}, e error) interface{} { return nothing }
-func ThrowException(e error) func() interface{} { return func() interface{} { panic(e) } }
-func CatchException(c func(error) func() interface{}, t func() interface{}) func() interface{} {
-	return func() (res interface{}) {
-		defer func() {
-			if r := recover(); r != nil {
-				if err, ok := r.(error); ok {
-					res = c(err)()
-				} else {
-					// Fallback if panic is not an error type
-					res = c(errors.New("panic"))()
-				}
-			}
-		}()
-		return t()
-	}
+
+func ThrowException(e error, _ interface{}) interface{} {
+	panic(e)
 }
+
+func CatchException(c func(error) func(interface{}) interface{}, t func(interface{}) interface{}, _ interface{}) (res interface{}) {
+	defer func() {
+		if r := recover(); r != nil {
+			if err, ok := r.(error); ok {
+				res = c(err)(nil)
+			} else {
+				res = c(errors.New("panic"))(nil)
+			}
+		}
+	}()
+	return t(nil)
+}
+
 func ErrorWithCause(msg string, cause error) error { return errors.New(msg) }
 func ErrorWithName(name string, msg string) error { return errors.New(msg) }
